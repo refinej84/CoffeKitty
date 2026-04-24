d73e139b157b0c092fee46d7c53f19bdb4f7f0fe
Daniel Campos <danielcamposborgessantiago6@gmail.com>
2026-04-24 13:49:50 -0300

    bug fix das imagens, agora funcionando no debug false

--- coffeekitty/urls.py
+++ coffeekitty/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +12,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 
     path('', include('cafeteria.urls')),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
